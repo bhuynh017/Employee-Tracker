87a256745ed6f427354ad1af9a3751b5758f375a
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-11-09 12:03:55 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-const express = require('express');
+const inquirer = require("inquirer");
 const cTable = require('console.table');
+const mysql = require('mysql2');
+
+const connection = mysql.connection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employeesDB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`Employee Tracker`)
+    firstPrompt();
+});
+
