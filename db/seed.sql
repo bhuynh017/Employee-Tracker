d4f4f84933a26386a6013fc5ec62b866d73fedaf
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-22 16:27:19 -0600

    adding values into the department insert

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,8 @@ use employees;
 /* Inserting data into the department table: data includes the name of the department that which employee is in */
 INSERT INTO department
 (name)
+VALLUES
+('SALES'),
+('ENGINEERING'),
+('FINANCE'),
+('LEGAL');
