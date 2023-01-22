8535c0a5afbcd2f265245d9502a78ae4dac060ac
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-22 16:22:58 -0600

    renaming seeds file into seed and adding the use employees on the first line to utilize the employee database. Creating the first INSERT INTO meaning to insert data into the department

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+/* using the employees database that was created */
+use employees;
+
+/* Inserting data into the department table: data includes the name of the department that which employee is in */
+INSERT INTO department
+(name)
