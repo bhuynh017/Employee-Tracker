0aa8c1f3c5b039ef5301298c395bfa50d59091d5
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-22 15:36:25 -0600

    adding logo text

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const db = require("./db");
 require("console.table");
 
 init();
+
+// Displaying logo text and loading in the main prompts
+function init() {
+    const firstText =({ name: "Employee Manager" }).render();
+
+    console.log(firstText);
+
+    loadMainPrompts();
+}
