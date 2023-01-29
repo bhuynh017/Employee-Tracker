d9adfe68ad4f7377c9bc616f14ae304371258ef4
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-29 11:27:37 -0600

    testing to see if application loads in correctly when adding the prompts

--- index.js
+++ index.js
@@ -14,3 +14,73 @@ function init() {
     loadMainPrompts();
   }
 
+  function loadMainPrompts() {
+    prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add employee",
+            "Add Department",
+            "Add Role",
+            "Remove employee",
+            "Update employee role",
+            "Update employee manager"
+        ]
+      }
+    ]).then(res => {
+      let choice = res.choice;
+      // Call the appropriate function depending on what the user chose
+      switch (choice) {
+        case "VIEW_EMPLOYEES":
+          viewEmployees();
+          break;
+        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+          viewEmployeesByDepartment();
+          break;
+        case "VIEW_EMPLOYEES_BY_MANAGER":
+          viewEmployeesByManager();
+          break;
+        case "ADD_EMPLOYEE":
+          addEmployee();
+          break;
+        case "REMOVE_EMPLOYEE":
+          removeEmployee();
+          break;
+        case "UPDATE_EMPLOYEE_ROLE":
+          updateEmployeeRole();
+          break;
+        case "UPDATE_EMPLOYEE_MANAGER":
+          updateEmployeeManager();
+          break;
+        case "VIEW_DEPARTMENTS":
+          viewDepartments();
+          break;
+        case "ADD_DEPARTMENT":
+          addDepartment();
+          break;
+        case "REMOVE_DEPARTMENT":
+          removeDepartment();
+          break;
+        case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
+          viewUtilizedBudgetByDepartment();
+          break;
+        case "VIEW_ROLES":
+          viewRoles();
+          break;
+        case "ADD_ROLE":
+          addRole();
+          break;
+        case "REMOVE_ROLE":
+          removeRole();
+          break;
+        default:
+          quit();
+      }
+    }
+    )
+  }
