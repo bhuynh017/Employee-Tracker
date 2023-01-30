7be9601d9808d997aef3d0c54f63c49dce93a2c8
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-29 20:37:32 -0600

    adding the addemployees function to make the application fully functional. Tested to make sure it is working properly.:

--- index.js
+++ index.js
@@ -473,6 +473,78 @@ function viewEmployeesByDepartment() {
           })
       }
 
+      // creating function to add a new employee.
+      function addEmployee() {
+        // displaying questions for the user to answer.
+        prompt([
+          {
+            name: "first_name",
+            message: "What is the employee's first name?"
+          },
+          {
+            name: "last_name",
+            message: "What is the employee's last name?"
+          }
+        ])
+          .then(res => {
+            let firstName = res.first_name;
+            let lastName = res.last_name;
+      
+            // queries the data and creates an array of choice from the returned data.
+            
+            db.findAllRoles()
+              .then(([rows]) => {
+                let roles = rows;
+                const roleChoices = roles.map(({ id, title }) => ({
+                  name: title,
+                  value: id
+                }));
+      
+                // asking further questions on what is the employee's role.
+                prompt({
+                  type: "list",
+                  name: "roleId",
+                  message: "What is the employee's role?",
+                  choices: roleChoices
+                })
+                  .then(res => {
+                    let roleId = res.roleId;
+      
+                    db.findAllEmployees()
+                      .then(([rows]) => {
+                        let employees = rows;
+                        const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+                          name: `${first_name} ${last_name}`,
+                          value: id
+                        }));
+      
+                        managerChoices.unshift({ name: "None", value: null });
+      
+                        prompt({
+                          type: "list",
+                          name: "managerId",
+                          message: "Who is the employee's manager?",
+                          choices: managerChoices
+                        })
+                          .then(res => {
+                            let employee = {
+                              manager_id: res.managerId,
+                              role_id: roleId,
+                              first_name: firstName,
+                              last_name: lastName
+                            }
+      
+                            db.createEmployee(employee);
+                          })
+                          .then(() => console.log(
+                            `Added ${firstName} ${lastName} to the database`
+                          ))
+                          .then(() => loadMainPrompts())
+                      })
+                  })
+              })
+          })
+      }
 
 // When the user exits the application.
   function quit() {
