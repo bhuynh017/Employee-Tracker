671cec9eccf9c836ca4426ac8dc61dc92480cb87
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-01-29 20:21:27 -0600

    updating lines 28-85 due to application not prompting the next action due to the name & value not being specified

--- index.js
+++ index.js
@@ -24,15 +24,66 @@ function init() {
         name: "choice",
         message: "What would you like to do?",
         choices: [
-            "View all employees",
-            "View all employees by department",
-            "View all employees by manager",
-            "Add employee",
-            "Add Department",
-            "Add Role",
-            "Remove employee",
-            "Update employee role",
-            "Update employee manager"
+            {
+                name: "View All Employees",
+                value: "VIEW_EMPLOYEES"
+              },
+              {
+                name: "View All Employees By Department",
+                value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+              },
+              {
+                name: "View All Employees By Manager",
+                value: "VIEW_EMPLOYEES_BY_MANAGER"
+              },
+              {
+                name: "Add Employee",
+                value: "ADD_EMPLOYEE"
+              },
+              {
+                name: "Remove Employee",
+                value: "REMOVE_EMPLOYEE"
+              },
+              {
+                name: "Update Employee Role",
+                value: "UPDATE_EMPLOYEE_ROLE"
+              },
+              {
+                name: "Update Employee Manager",
+                value: "UPDATE_EMPLOYEE_MANAGER"
+              },
+              {
+                name: "View All Roles",
+                value: "VIEW_ROLES"
+              },
+              {
+                name: "Add Role",
+                value: "ADD_ROLE"
+              },
+              {
+                name: "Remove Role",
+                value: "REMOVE_ROLE"
+              },
+              {
+                name: "View All Departments",
+                value: "VIEW_DEPARTMENTS"
+              },
+              {
+                name: "Add Department",
+                value: "ADD_DEPARTMENT"
+              },
+              {
+                name: "Remove Department",
+                value: "REMOVE_DEPARTMENT"
+              },
+              {
+                name: "View Total Utilized Budget By Department",
+                value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
+              },
+              {
+                name: "Quit",
+                value: "QUIT"
+              }
         ]
       }
     ]).then(res => {
@@ -243,6 +294,54 @@ function viewEmployeesByDepartment() {
               });
           })
       }
+
+      // updates the EmployeeManager
+      function updateEmployeeManager() {
+        // retrieving a list of employees from the DB
+        db.findAllEmployees()
+          .then(([rows]) => {
+            let employees = rows;
+            const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id
+            }));
+      
+            // prompting the list and message in the choices employeeChoices.
+            prompt([
+              {
+                type: "list",
+                name: "employeeId",
+                message: "Which employee's manager do you want to update?",
+                choices: employeeChoices
+              }
+            ])
+              .then(res => {
+                let employeeId = res.employeeId;
+                db.findAllPossibleManagers(employeeId)
+                  .then(([rows]) => {
+                    let managers = rows;
+                    const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+                      name: `${first_name} ${last_name}`,
+                      value: id
+                    }));
+      
+                    prompt([
+                      {
+                        type: "list",
+                        name: "managerId",
+                        message:
+                          "Which employee do you want to set as manager for the selected employee?",
+                        choices: managerChoices
+                      }
+                    ])
+                      .then(res => db.updateEmployeeManager(employeeId, res.managerId))
+                      .then(() => console.log("Updated employee's manager"))
+                      .then(() => loadMainPrompts());
+                  });
+              });
+          });
+      }
+      
       
 // When the user exits the application.
   function quit() {
